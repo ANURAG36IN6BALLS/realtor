502ab0ebca2f015885c28fdd6d6637629154ca9d
ANURAG36IN6BALLS <anurag7060721985@gmail.com>
2023-01-17 14:42:33 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from './pages/Home';
+import Offers from './pages/Offers';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Forgotpassword from './pages/Forgotpassword';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Offers" element={<Offers />} />
+          <Route path="/Signin" element={< Signin/>} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Forgotpassword" element={< Forgotpassword/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
