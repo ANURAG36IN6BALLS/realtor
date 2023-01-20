bfbf7bd0a898a21effe19bde3791a084b380ec25
ANURAG36IN6BALLS <anurag7060721985@gmail.com>
2023-01-20 12:36:30 +0530

    Designed The Basic Form

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -3,29 +3,23 @@ import React from 'react'
 export default function Signin() {
   return (
     <section>
-      <h1 className=' font-bold text-center py-10 text-[2rem]'>sign in</h1>
-      <div className='max-w-6xl  flex flex-wrap'>
-        <div className='lg:w-[45%] md:w-[67%] mx-auto '>
+      <h1 className=' font-bold text-center py-10 text-[2rem]'>Sign In</h1>
+      <div className='flex flex-wrap justify-center max-w-6xl mx-6 items-center'>
+        <div className='md:w-[67%] lg:w-[50%] md:mb-8 lg:mb-4 sm:mb-10'>
           <img 
           src="./signin.jpg" alt=""image 
-          className='w-full mx-4 rounded-2xl'
+          className='w-full rounded-2xl'
           />
 
         </div>
-        <div className='lg:w-[45%] my-auto lg:mx-10 md:w-[67%] mx-auto '>
-
+        <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-10'>
+    
           <form action="">
 
               <input 
               type="text" 
-              className='w-full outline-offset-50 outline-blue-800  my-2 py-1 px-20 font-bold rounded-lg border-black border-2'
+              className='w-full rounded-lg px-4 py-2'
               placeholder='E-mail Address'
-              />
-              <input 
-              type="password"
-              className='w-full outline-offset-4  outline-blue-800 my-2 py-1 px-20 font-bold rounded-lg border-black border-2 '
-              placeholder='Password'
-              
               />
 
           </form>
